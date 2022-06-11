1111183e9c0aee1e4e1aefc5471c0e1b55869da3
debus3 <debus3@gmail.com>
2022-06-11 23:07:28 +0200

    add main page redirect

--- myblog/urls.py
+++ myblog/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 app_name = 'photoblog'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('photoblog/',include('photoblog.urls'))
+    path('photoblog/',include('photoblog.urls')),
+    path('',RedirectView.as_view(url='photoblog/')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
